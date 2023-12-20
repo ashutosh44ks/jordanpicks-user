fbacc52096a32b1e34f3f992f0e7b3b385e81a44
Ashutosh Kumar Singh <ashutosh44ks@gmail.com>
2023-12-21 03:17:23 +0530

    Add About page content

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <div>
+      <h2 className="text-center mb-8">
+        Jordanspicks Puts Integrity First in Sports Betting
+      </h2>
+      <p className="my-4 text-center">
+        Sports betting is a complex game that emerged from the days when Las
+        Vegas was one of the only places in the US where you could legally place
+        bets. In this shadowy world, sports bettors have often been swindled by
+        lofty promises and outright lies. As betting becomes more mainstream,
+        dangers still lurk in the shadows.
+      </p>
+      <p className="my-4 text-center">
+        At Jordanspicks, we take a different approach. We don’t upsell, cram
+        different packages down your throat, or treat you like a piece of meat.
+        Our goal is to provide a straightforward and transparent experience for
+        sports bettors.
+      </p>
+    </div>
+  );
+};
 
-export default About
+export default About;
