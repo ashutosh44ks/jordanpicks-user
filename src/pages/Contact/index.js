d032af1eb96d27fda611d8b3aa11f4d64ff6cbc4
Ashutosh Kumar Singh <ashutosh44ks@gmail.com>
2023-12-21 03:14:43 +0530

    add Contact page content

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -1,9 +1,105 @@
-import React from 'react'
+import { useState } from "react";
+import api from "../../components/utils/api";
+import myToast from "../../components/utils/myToast";
+import Button from "../../components/common/Button";
 
 const Contact = () => {
-  return (
-    <div>Contact</div>
-  )
-}
+  const [fName, setFName] = useState("");
+  const [lName, setLName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [msg, setMsg] = useState("");
 
-export default Contact
+  const sendMsg = async () => {
+    try {
+      const { data } = await api.post("/user/contact", {
+        fName,
+        lName,
+        email,
+        mobile: phone,
+        message: msg,
+      });
+      myToast(data.msg, "success");
+    } catch (err) {
+      console.log(err);
+      myToast(err?.response?.data?.error || "Something went wrong", "failure");
+    }
+  };
+  return (
+    <div>
+      <div>
+        <h2 className="text-center mb-4">How can we help you?</h2>
+        <h4 className="text-center">Get In Touch!</h4>
+      </div>
+      <form
+        className="my-8"
+        onSubmit={(e) => {
+          e.preventDefault();
+          sendMsg();
+        }}
+      >
+        <div className="mb-4">
+          <input
+            className="min-w-[20rem] w-full"
+            type="text"
+            value={fName}
+            onChange={(e) => setFName(e.target.value)}
+            placeholder="First Name"
+            required
+          />
+        </div>
+        <div className="mb-4">
+          <input
+            className="min-w-[20rem] w-full"
+            type="text"
+            value={lName}
+            onChange={(e) => setLName(e.target.value)}
+            placeholder="Last Name"
+            required
+          />
+        </div>
+        <div className="mb-4">
+          <input
+            className="min-w-[20rem] w-full"
+            type="text"
+            value={phone}
+            onChange={(e) => {
+              if (!isNaN(e.target.value)) setPhone(e.target.value);
+            }}
+            maxLength={10}
+            placeholder="Mobile No."
+            required
+          />
+        </div>
+        <div className="mb-4">
+          <input
+            className="min-w-[20rem] w-full"
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Email"
+            required
+          />
+        </div>
+        <div className="mb-4">
+          <textarea
+            className="min-w-[20rem] w-full"
+            type="text"
+            value={msg}
+            onChange={(e) => setMsg(e.target.value)}
+            placeholder="Message"
+            rows={8}
+            required
+          />
+        </div>
+        <div>
+          <Button theme="pink" type="submit" rounded="none" className="w-full">
+            Send
+          </Button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Contact;
