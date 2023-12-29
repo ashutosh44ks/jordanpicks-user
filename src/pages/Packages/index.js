38de3aa3fab7249f3d796e61f216af3f0e73e37b
Ashutosh <ashutoshks@digitalnightowl.in>
2023-12-29 18:50:41 +0530

    add additional loader to package cards

--- src/pages/Packages/index.js
+++ src/pages/Packages/index.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import Banner from "./components/Banner";
 import api from "../../components/utils/api";
 import PackageBox from "./components/PackageBox";
+import { RiLoader4Line } from "react-icons/ri";
 import "./packages.css";
 
 const Packages = () => {
@@ -72,7 +73,9 @@ const Packages = () => {
         <div className="my-16 flex flex-wrap gap-x-8 gap-y-16 justify-center">
           {loading
             ? [1, 2, 3, 4].map((item) => (
-                <div className="package-box skeleton" key={item}></div>
+                <div className="package-box skeleton" key={item}>
+                  <RiLoader4Line className="text-4xl animate-spin text-grey" />
+                </div>
               ))
             : packages.map((item) => <PackageBox item={item} key={item._id} />)}
         </div>
