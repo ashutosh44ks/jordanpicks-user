69fd0f5df8178f72d4ae9db20c01c980d54c6abf
Ashutosh <ashutoshks@digitalnightowl.in>
2024-02-05 16:02:14 +0530

    fix unequal sizes of package boxes

--- src/pages/Packages/components/PackageBox.js
+++ src/pages/Packages/components/PackageBox.js
@@ -16,12 +16,14 @@ const PackageBox = ({ item }) => {
         {item._id ? diffTimeData.diffSec : 0} secs left
       </div>
       <div className="package-box-body">
-        <div className="flex justify-center items-start mb-4">
-          <h4 className="mt-1">$</h4>
-          <h1 className="text-pink">{item.price.toFixed(2)}</h1>
+        <div className="h-[12rem] flex flex-col justify-center">
+          <div className="flex justify-center items-start mb-4">
+            <h4 className="mt-1">$</h4>
+            <h1 className="text-pink">{item.price.toFixed(2)}</h1>
+          </div>
+          <h2 className="text-center limit-to-2-lines">{item.name}</h2>
+          <h5 className="text-center mt-4 limit-to-2-lines">{item.gamePreview}</h5>
         </div>
-        <h2 className="text-center">{item.name}</h2>
-        <h5 className="text-center my-4">{item.gamePreview}</h5>
         <hr className="my-6 mx-6 border-black" />
         <div
           className="text-center limit-to-5-lines break-words h-[6rem]"
