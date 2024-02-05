056e2ef3460b55fb2caa8625ec0f30e1b5a3d5e8
Ashutosh <ashutoshks@digitalnightowl.in>
2024-02-05 16:11:42 +0530

    remove 2nd banner

--- src/pages/Packages/components/Banner.js
+++ src/pages/Packages/components/Banner.js
@@ -1,37 +1,37 @@
-import { useState, useEffect } from "react";
+// import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useWindowWidth } from "../../../components/utils/useWindowWidth";
-import { TbCircleFilled } from "react-icons/tb";
+// import { TbCircleFilled } from "react-icons/tb";
 
-const BtnGrp = ({ activeIndex }) => {
-  return (
-    <div className="flex items-center justify-center gap-2 my-4 text-xs">
-      <TbCircleFilled
-        className={activeIndex === 0 ? "text-pink" : "text-white"}
-      />
-      <TbCircleFilled
-        className={activeIndex === 1 ? "text-pink" : "text-white"}
-      />
-    </div>
-  );
-};
+// const BtnGrp = ({ activeIndex }) => {
+//   return (
+//     <div className="flex items-center justify-center gap-2 my-4 text-xs">
+//       <TbCircleFilled
+//         className={activeIndex === 0 ? "text-pink" : "text-white"}
+//       />
+//       <TbCircleFilled
+//         className={activeIndex === 1 ? "text-pink" : "text-white"}
+//       />
+//     </div>
+//   );
+// };
 
 const Banner = () => {
   const navigate = useNavigate();
   const width = useWindowWidth();
 
-  const [activeIndex, setActiveIndex] = useState(0);
-  const [hover, setHover] = useState(false);
-  useEffect(() => {
-    const myInterval = setInterval(() => {
-      if (!hover) setActiveIndex(activeIndex === 0 ? 1 : 0);
-    }, 4000);
-    return () => {
-      clearInterval(myInterval);
-    };
-  }, [activeIndex]);
+  // const [activeIndex, setActiveIndex] = useState(0);
+  // const [hover, setHover] = useState(false);
+  // useEffect(() => {
+  //   const myInterval = setInterval(() => {
+  //     if (!hover) setActiveIndex(activeIndex === 0 ? 1 : 0);
+  //   }, 4000);
+  //   return () => {
+  //     clearInterval(myInterval);
+  //   };
+  // }, [activeIndex]);
 
-  const commonStyle = "transform transition-transform duration-500 ease-in-out";
+  // const commonStyle = "transform transition-transform duration-500 ease-in-out";
 
   if (width < 480) {
     return (
@@ -39,7 +39,7 @@ const Banner = () => {
         <img
           src="/assets/slider-mobile.png"
           alt="banner-mobile"
-          className={`${commonStyle} cursor-pointer w-full`}
+          className="cursor-pointer w-full"
           onClick={() => navigate("/auth/register")}
         />
       </div>
@@ -48,30 +48,40 @@ const Banner = () => {
 
   return (
     <div className="transform -translate-y-16">
-      <div
-        className="overflow-hidden flex items-center"
-        onMouseEnter={() => setHover(true)}
-        onMouseLeave={() => setHover(false)}
-      >
-        <img
-          src="/assets/slider0.png"
-          alt="banner0"
-          className={`${commonStyle} cursor-pointer ${
-            activeIndex === 0 ? "translate-x-0" : "-translate-x-full"
-          }`}
-          onClick={() => navigate("/auth/register")}
-        />
-        <img
-          src="/assets/slider1.png"
-          alt="banner1"
-          className={`${commonStyle} ${
-            activeIndex === 1 ? "-translate-x-full" : "translate-x-0"
-          }`}
-        />
-      </div>
-      <BtnGrp activeIndex={activeIndex} setActiveIndex={setActiveIndex} />
+      <img
+        src="/assets/slider0.png"
+        alt="banner0"
+        className="cursor-pointer w-full"
+        onClick={() => navigate("/auth/register")}
+      />
     </div>
   );
+  // return (
+  //   <div className="transform -translate-y-16">
+  //     <div
+  //       className="overflow-hidden flex items-center"
+  //       onMouseEnter={() => setHover(true)}
+  //       onMouseLeave={() => setHover(false)}
+  //     >
+  //       <img
+  //         src="/assets/slider0.png"
+  //         alt="banner0"
+  //         className={`${commonStyle} cursor-pointer ${
+  //           activeIndex === 0 ? "translate-x-0" : "-translate-x-full"
+  //         }`}
+  //         onClick={() => navigate("/auth/register")}
+  //       />
+  //       <img
+  //         src="/assets/slider1.png"
+  //         alt="banner1"
+  //         className={`${commonStyle} ${
+  //           activeIndex === 1 ? "-translate-x-full" : "translate-x-0"
+  //         }`}
+  //       />
+  //     </div>
+  //     <BtnGrp activeIndex={activeIndex} setActiveIndex={setActiveIndex} />
+  //   </div>
+  // );
 };
 
 export default Banner;
