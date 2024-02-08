e2170d7e1ab0ba04215d3f361108423281e0fcdf
Ashutosh <ashutoshks@digitalnightowl.in>
2024-02-08 16:31:33 +0530

    add store page - untested

---
+++ src/pages/Store/Payment.js
@@ -0,0 +1,104 @@
+import { useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import api from "../../components/utils/api";
+import { RiCheckboxCircleLine } from "react-icons/ri";
+import { RxCrossCircled } from "react-icons/rx";
+import { GoDotFill } from "react-icons/go";
+
+const Payment = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  const searchParams = new URLSearchParams(location.search);
+  const paymentIntentId = searchParams.get("payment_intent");
+  const cardDeduction = searchParams.get("cardDeduction");
+  const webCredit = searchParams.get("webCredit");
+  const [status, setStatus] = useState("");
+  const validatePayment = async () => {
+    try {
+      const { data } = await api.post("/user/validatePaymentPackage", {
+        paymentIntentId: paymentIntentId,
+        webCredit,
+        cardDeduction,
+      });
+      console.log(data);
+      setStatus(data.status);
+      setTimeout(() => {
+        navigate("/my-account/transactions");
+      }, 5000);
+    } catch (error) {
+      console.log(error);
+      setStatus("failed");
+    }
+  };
+  const [numberOfDots, setNumberOfDots] = useState(0);
+  useEffect(() => {
+    validatePayment();
+    const myInterval = setInterval(() => {
+      setNumberOfDots((numberOfDots) => (numberOfDots + 1) % 4);
+    }, 1000);
+    return () => {
+      clearInterval(myInterval);
+    };
+  }, []);
+
+  return (
+    <div className="flex flex-col items-center">
+      <h2 className="mb-8">
+        Payment <span className="text-pink">Validation</span>
+      </h2>
+      {status === "" ? (
+        <div>
+          <h3 className="flex justify-center mb-2">
+            <GoDotFill className="text-pink" />
+            <GoDotFill
+              className={`text-pink ${
+                numberOfDots >= 1 ? "opacity-100" : "opacity-50"
+              }`}
+            />
+            <GoDotFill
+              className={`text-pink ${
+                numberOfDots >= 2 ? "opacity-100" : "opacity-50"
+              }`}
+            />
+            <GoDotFill
+              className={`text-pink ${
+                numberOfDots >= 3 ? "opacity-100" : "opacity-50"
+              }`}
+            />
+          </h3>
+          <p className="text-center">
+            Please wait while we validate your payment.
+          </p>
+          <p className="text-center">Do NOT refresh the page or go back.</p>
+        </div>
+      ) : status === "succeeded" ? (
+        <div>
+          <h3 className="font-medium flex justify-center items-center gap-2 mb-2">
+            <RiCheckboxCircleLine className="text-pink" />
+            Payment Successful
+          </h3>
+          <p className="text-center text-lightgrey2">
+            You will be redirected to your dashboard shortly.
+          </p>
+        </div>
+      ) : (
+        <div>
+          <h3 className="font-medium flex justify-center items-center gap-2 mb-2">
+            <RxCrossCircled className="text-pink" />
+            Payment Failed
+          </h3>
+          <p className="text-center text-lightgrey2">
+            Please copy the url and mention it while you raise an issue in the
+            support page.
+          </p>
+          <p className="text-center text-lightgrey2">
+            We will get back to you shortly.
+          </p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Payment;
---
+++ src/pages/Store/index.js
@@ -0,0 +1,84 @@
+import { useState, useRef } from "react";
+import api from "../../components/utils/api";
+import myToast from "../../components/utils/myToast";
+import Button from "../../components/common/Button";
+import Modal from "../../components/common/Modal";
+import Stripe from "./components/Stripe";
+
+const Store = () => {
+  const packages = [
+    {
+      name: "Web Credit Bronze",
+      price: 98.95,
+      credit: 150,
+    },
+    {
+      name: "Web Credit Silver",
+      price: 198.95,
+      credit: 400,
+    },
+    {
+      name: "Web Credit Gold",
+      price: 498.95,
+      credit: 1350,
+    },
+  ];
+
+  const dialogRef = useRef(null);
+  const [openModal, setOpenModal] = useState({});
+
+  return (
+    <>
+      <div>
+        <h3 className="mb-4 sm:text-left text-center">Store</h3>
+        <p className="sm:text-left text-center">
+          Exchange your money for credits. Credits can be used to purchase
+          packages on our platform.
+        </p>
+        <div className="my-8 flex flex-wrap gap-8">
+          {packages.map((p, i) => (
+            <div key={i} className="p-6 my-4 rounded-lg bg-dark2">
+              <h2 className="mb-4 text-center">{p.name}</h2>
+              <div className="flex justify-center items-start mb-4">
+                <h4 className="mt-1">$</h4>
+                <h1 className="text-pink">{p.price}</h1>
+              </div>
+              <hr className="my-6 mx-6 border-black" />
+              <h5 className="my-4 font-normal text-center">
+                You'll get {p.credit} web credit
+              </h5>
+              <Button
+                theme="pink"
+                size="md-rect"
+                className="w-full"
+                onClick={() => {
+                  setOpenModal(p);
+                }}
+              >
+                Buy
+              </Button>
+            </div>
+          ))}
+        </div>
+      </div>
+      {openModal.price > 0 && (
+        <Modal
+          ref={dialogRef}
+          title="Pay with Card"
+          content={
+            <Stripe
+              webCredit={openModal.credit}
+              cardDeduction={openModal.price}
+            />
+          }
+          closeDialog={() => {
+            dialogRef.current.closeDialog();
+            setOpenModal({});
+          }}
+        />
+      )}
+    </>
+  );
+};
+
+export default Store;
