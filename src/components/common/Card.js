ea3a7b069d9ebf2e9e98b000b6288b41da344bae
Ashutosh <ashutoshks@digitalnightowl.in>
2024-02-15 17:29:05 +0530

    remove unused components

--- src/components/common/Card.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./card.css";
-
-const Card = ({ children, className = "", size = "md" }) => {
-  return <div className={`card ${className} card-${size}`}>{children}</div>;
-};
-
-export default Card;
