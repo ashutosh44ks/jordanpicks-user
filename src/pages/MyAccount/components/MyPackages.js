d5151654e0f124a345ca8ec28ae40997d8d602ba
Ashutosh <ashutoshks@digitalnightowl.in>
2024-02-28 16:08:25 +0530

    update my packages UI on smaller screens

--- src/pages/MyAccount/components/MyPackages.js
+++ src/pages/MyAccount/components/MyPackages.js
@@ -1,29 +1,13 @@
 import { useState, useEffect } from "react";
+import { useWindowWidth } from "../../../components/utils/useWindowWidth";
 import api from "../../../components/utils/api";
 import Table from "../../../components/common/Table";
 import Pagination from "../../../components/common/Pagination";
+import { PiLinkSimple } from "react-icons/pi";
+import { MdOutlineArrowRight } from "react-icons/md";
 
-const MyPackages = () => {
-  const [myPackages, setMyPackages] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [page, setPage] = useState(1);
-  const [totalPages, setTotalPages] = useState(0);
-  const getMyPackages = async () => {
-    setLoading(true);
-    try {
-      const { data } = await api.get(`/user/getMyPackages?page=${page}`);
-      console.log(data);
-      setMyPackages(data.dta);
-      setTotalPages(data.totalPages);
-    } catch (error) {
-      console.log(error);
-    }
-    setLoading(false);
-  };
-  useEffect(() => {
-    getMyPackages();
-  }, [page]);
-
+const RenderMyPackagesData = ({ myPackages, loading }) => {
+  const width = useWindowWidth();
   const updateResultTerminalogy = (result) => {
     switch (result) {
       case "win":
@@ -32,15 +16,13 @@ const MyPackages = () => {
         return "unprofitable";
       case "tie":
         return "break even";
-      case undefined:
-        return "-";
       default:
         return result;
     }
   };
 
-  return (
-    <div>
+  if (width > 768)
+    return (
       <Table
         tHead={["S.No.", "Package Name", "Bets/Links", "Result"]}
         wrapperClass="my-8"
@@ -79,6 +61,74 @@ const MyPackages = () => {
           </tr>
         )}
       </Table>
+    );
+
+  return myPackages.map((p, index) => (
+    <div className="bg-dark p-4 my-4 rounded-lg">
+      <div className="flex justify-between items-center mb-2 flex-col xs:flex-row gap-x-4 gap-y-1">
+        <h4 className="text-yellow">{p.name}</h4>
+        {p.result ? (
+          <span className="uppercase tracking-wider text-sm">
+            {updateResultTerminalogy(p.result)}
+          </span>
+        ) : (
+          <span className="uppercase tracking-wider text-sm">
+            {p.discount}% DISCOUNT
+          </span>
+        )}
+      </div>
+      <div className="flex items-center flex-col xs:items-start">
+        {p.bets !== undefined &&
+          p.bets.length > 0 &&
+          p.bets.map((b, index) => (
+            <p key={index} className="flex items-center gap-2">
+              <MdOutlineArrowRight />
+              {b}
+            </p>
+          ))}
+        {p.links !== undefined &&
+          p.links.length > 0 &&
+          p.links.map((l) => (
+            <a
+              className="flex items-center gap-2"
+              href={l}
+              key={index}
+              rel="noreferrer"
+              target="_blank"
+            >
+              <PiLinkSimple />
+              {l}
+            </a>
+          ))}
+      </div>
+    </div>
+  ));
+};
+
+const MyPackages = () => {
+  const [myPackages, setMyPackages] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
+  const getMyPackages = async () => {
+    setLoading(true);
+    try {
+      const { data } = await api.get(`/user/getMyPackages?page=${page}`);
+      console.log(data);
+      setMyPackages(data.dta);
+      setTotalPages(data.totalPages);
+    } catch (error) {
+      console.log(error);
+    }
+    setLoading(false);
+  };
+  useEffect(() => {
+    getMyPackages();
+  }, [page]);
+
+  return (
+    <div>
+      <RenderMyPackagesData myPackages={myPackages} loading={loading} />
       <Pagination lastPage={totalPages} page={page} setPage={setPage} />
     </div>
   );
